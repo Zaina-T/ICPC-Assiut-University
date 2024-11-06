8c7e2da5082993b4da4e88638aa20144b9163f0e
Giorgi Mshvidobadze <g_mshvidobadze1@cu.edu.ge>
2024-11-06 19:06:34 +0400

    I. Welcome for you with Conditions

---
+++ Sheet #1 (Data type - Conditions)/I. Welcome for you with Conditions/solution.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int a,b;
+ 
+int main()
+{
+    cin >> a >> b;
+    if ( a >= b)
+        cout << "Yes" << endl;
+    else
+        cout <<"No" << endl;
+    
+}
