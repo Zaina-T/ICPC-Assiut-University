d5d4238279351570637f28bfdc935fa4779ba516
Giorgi Mshvidobadze <g_mshvidobadze1@cu.edu.ge>
2024-11-06 18:39:55 +0400

    C. Simple Calculator

---
+++ Sheet #1 (Data type - Conditions)/C. Simple Calculator/solution.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+ 
+long long  x , y;
+ 
+int main() {
+   cin >> x >> y;
+   
+   cout << x << " + " << y << " = " << x+y << endl;
+   cout << x << " * " << y << " = " << x*y << endl;
+   cout << x << " - " << y << " = " << x-y << endl;
+}
